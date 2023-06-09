4bd303d6c53845fc874ba6f095eb81070e7d9ebf
Kyeongtak (David) Han <93216105+Han00127@users.noreply.github.com>
2023-06-09 14:19:42 +0900

    Update lmdesign.sh

--- scripts/lmdesign.sh
+++ scripts/lmdesign.sh
@@ -1,26 +1,30 @@
 #!/bin/bash
-# out_folder='./results/lmdesign1_reproduce'
-# cathDataPath='./data/chain_set.jsonl'
-# splits='./data/chain_set_splits.json'
-# shortPath='./data/test_split_L100.json'
-# scPath='./data/test_split_sc.json'
-# pretrained_weights='/data/project/rw/LMDesign_weights/mpnn_cmlm_trained_weight.pt' # if you can access to braincloud project dir, use this for pretrained MPNN+CMLM
+out_folder='./results/lmdesign1_reproduce'
+cathDataPath='./data/chain_set.jsonl'
+splits='./data/chain_set_splits.json'
+shortPath='./data/test_split_L100.json'
+scPath='./data/test_split_sc.json'
+pretrained_weights='/data/project/rw/LMDesign_weights/mpnn_cmlm_trained_weight.pt' # if you can access to braincloud project dir, use this for pretrained MPNN+CMLM
 
-# ## Single implementation if make separated shell script for each experiment like lmdesign1.sh lmdesign2.sh lmdesign3.sh with given scripts
-# # Training LMDESIGN on CATH4.2 exp1 
-# python lmdesign_train.py --epoch 1 --out_folder $out_folder --jsonl_path $cathDataPath --file_splits $splits --test_short_path $shortPath --test_single_path $scPath \
-#                          --embed_dim 1280 --num_heads 10 --structure_model MPNN --structure_trainable True --num_decoder_layers 4  --structure_weight ""
+####################################
+## Single implmenation 
+#####################################
+## Single implementation if make separated shell script for each experiment like lmdesign1.sh lmdesign2.sh lmdesign3.sh with given scripts
+# Training LMDESIGN on CATH4.2 exp1 
+python lmdesign_train.py --epoch 1 --out_folder $out_folder --jsonl_path $cathDataPath --file_splits $splits --test_short_path $shortPath --test_single_path $scPath \
+                         --embed_dim 1280 --num_heads 10 --structure_model MPNN --structure_trainable True --num_decoder_layers 4  --structure_weight ""
 
-# # Training LMDESIGN on CATH4.2 exp2
-# python lmdesign_train.py --epoch 1 --out_folder $out_folder --jsonl_path $cathDataPath --file_splits $splits --test_short_path $shortPath --test_single_path $scPath \
-#                         --embed_dim 1280 --num_heads 10 --structure_model MPNN --structure_trainable True --structure_weight $pretrained_weights --num_decoder_layers 4
+# Training LMDESIGN on CATH4.2 exp2
+python lmdesign_train.py --epoch 1 --out_folder $out_folder --jsonl_path $cathDataPath --file_splits $splits --test_short_path $shortPath --test_single_path $scPath \
+                        --embed_dim 1280 --num_heads 10 --structure_model MPNN --structure_trainable True --structure_weight $pretrained_weights --num_decoder_layers 4
 
-# # Training LMDESIGN on CATH4.2 exp3
-# python lmdesign_train.py --epoch 10 --out_folder$out_folder --jsonl_path $cathDataPath --file_splits $splits --test_short_path $shortPath --test_single_path $scPath \
-#                          --embed_dim 1280 --num_heads 10 --structure_model MPNN --structure_trainable False --structure_weight $pretrained_weights --num_decoder_layers 4
+# Training LMDESIGN on CATH4.2 exp3
+python lmdesign_train.py --epoch 10 --out_folder$out_folder --jsonl_path $cathDataPath --file_splits $splits --test_short_path $shortPath --test_single_path $scPath \
+                         --embed_dim 1280 --num_heads 10 --structure_model MPNN --structure_trainable False --structure_weight $pretrained_weights --num_decoder_layers 4
 
-
+#######################################
 ## V100 32G -> exp1 & exp2 are possibly parallely executed -> This makes a bit faster results.
+########################################
 cathDataPath='./data/chain_set.jsonl'
 splits='./data/chain_set_splits.json'
 shortPath='./data/test_split_L100.json'
