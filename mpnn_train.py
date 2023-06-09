ec7e1936ea98410e4892f24758de1a4e38e235dd
Kyeongtak (David) Han <93216105+Han00127@users.noreply.github.com>
2023-06-09 14:05:21 +0900

    Update mpnn_train.py

--- mpnn_train.py
+++ mpnn_train.py
@@ -101,6 +101,7 @@ def main(args):
     optimizer = get_std_opt(model.parameters(), args.hidden_dim, total_step)
     if args.resume:
         optimizer.optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+        
 
     ## Start training
     for e in range(args.epoch):
@@ -111,7 +112,7 @@ def main(args):
         train_pbar = tqdm(loader_train)
         for _, batch in enumerate(train_pbar):
             start_batch = time.time()
-            X, S, mask, lengths, chain_M, chain_encoding_all, chain_list_list, visible_list_list, masked_list_list, masked_chain_length_list_list, chain_M_pos, omit_AA_mask, residue_idx, dihedral_mask, tied_pos_list_of_lists_list, pssm_coef, pssm_bias, pssm_log_odds_all, bias_by_res_all, tied_beta = CATH_tied_featurize(batch, device, chain_id_dict)
+            X, S, mask, _, chain_M, chain_encoding_all, _, _, _, _, chain_M_pos, _, residue_idx, _, _, _, _, _, _, _ = CATH_tied_featurize(batch, device, chain_id_dict)
             elapsed_featurize = time.time() - start_batch
             optimizer.zero_grad()
             mask_for_loss = mask*chain_M
@@ -122,10 +123,11 @@ def main(args):
                     _, loss_av_smoothed = graph_loss_smoothed(S, log_probs, mask_for_loss)
             elif args.model == 'MPNN_CMLM':
                 _, mask_ = s_encoder_cmlm_mask(S,mask) # tokens 그대로, mask_ S_masked 
-                log_probs,_ = model(X, S, mask, chain_M*chain_M_pos, residue_idx, chain_encoding_all, randn_1)
-                newS = torch.masked_select(S, mask_.bool())
-                newP = log_probs[mask_.bool(),:]
-                loss, loss_av_smoothed = graph_loss_smoothed(newS, newP, mask_)
+                with torch.cuda.amp.autocast():
+                    log_probs,_ = model(X, S, mask, chain_M*chain_M_pos, residue_idx, chain_encoding_all, randn_1)
+                    newS = torch.masked_select(S, mask_.bool())
+                    newP = log_probs[mask_.bool(),:]
+                    loss, loss_av_smoothed = graph_loss_smoothed(newS, newP, mask_)
 
             scaler.scale(loss_av_smoothed).backward()  
             if args.gradient_norm > 0.0:
@@ -141,7 +143,6 @@ def main(args):
             total_step += 1
 
         t1 = time.time()
-        print("Times for one epoch ", t1 - t0)
 
         model.eval()
         with torch.no_grad():
@@ -227,9 +228,9 @@ def main(args):
         writer.add_scalar('Train perplexity', train_perplexity, e)
         writer.add_scalar('Validation perplexity', validation_perplexity, e)
         writer.add_scalar('Test perplexity', test_perplexity, e)
-        writer.add_scalar('Short perplexity', short_test_perplexity, e)
-        writer.add_scalar('Short median recovery', short_recovery, e)
-        writer.add_scalar('Single perplexity', sc_test_perplexity, e)
+        writer.add_scalar('Test Short perplexity', short_test_perplexity, e)
+        writer.add_scalar('Test Short median recovery', short_recovery, e)
+        writer.add_scalar('Test Single Chain perplexity', sc_test_perplexity, e)
         writer.add_scalar('Single median recovery', sc_recovery, e)
         output = "Epoch : {} Train perplexity : {:.3f} Valid perplexity : {:.3f} Test perplexity : {:.3f} \
                   Short perplexity {:.3f} Short median recovery {:.3f} Single perplexity {:.3f} \
@@ -239,7 +240,7 @@ def main(args):
         with open(os.path.join(base_folder,'log.txt'), 'a') as f:
             f.write(output+'\n')
 
-        checkpoint_filename = base_folder+'model_weights/last{}.pt'.format(e+1)
+        checkpoint_filename = base_folder+'model_weights/epoch{}.pt'.format(e+1)
         torch.save({
                 'epoch': e+1,
                 'step': total_step,
@@ -260,15 +261,15 @@ if __name__ == "__main__":
     argparser.add_argument("--epoch", type=int, default=100, help="training epochs")
     argparser.add_argument("--batch_size", type=int, default=6000, help="Batch size; can set higher for titan, quadro GPUs, reduce this if running out of GPU memory")
     argparser.add_argument("--max_length", type=int, default=200000, help="Max sequence length")
-    argparser.add_argument("--gradient_norm", type=float, default=1.0, help="gradient norm")
+    argparser.add_argument("--gradient_norm", type=float, default=0.0, help="gradient norm")
     argparser.add_argument("--mixed_precision", type=bool, default=True, help="train with mixed precision")
-    argparser.add_argument("--seed", type=int, default=2020, help="If set to 0 then a random seed will be picked;")
+    argparser.add_argument("--seed", type=int, default=0, help="If set to 0 then a random seed will be picked;")
 
     argparser.add_argument("--hidden_dim", type=int, default=128, help="hidden model dimension")
     argparser.add_argument("--num_layers", type=int, default=3, help="number of encoder layers") 
     argparser.add_argument("--num_neighbors", type=int, default=48, help="number of neighbors for the sparse graph")   
     argparser.add_argument("--dropout", type=float, default=0.1, help="dropout level; 0.0 means no dropout")
-    argparser.add_argument("--backbone_noise", type=float, default=0.2, help="amount of noise added to backbone during training") 
+    argparser.add_argument("--backbone_noise", type=float, default=0.0, help="amount of noise added to backbone during training") 
     
     argparser.add_argument("--out_folder", type=str, default='/data/project/rw/mpnn_results/MPNN/', help="Path to a folder to output sequences, e.g. /home/out/")
     argparser.add_argument("--jsonl_path", type=str,default='/data/project/rw/cath4.2/chain_set.jsonl',help="Path to parsed pdb into jsonl")
@@ -279,51 +280,3 @@ if __name__ == "__main__":
  
     args = argparser.parse_args()    
     main(args)
-
-
-
-        # with torch.no_grad():
-        #     short_test_sum, short_test_weights = 0., 0.
-        #     short_test_recovery = []
-        #     test_pbar = tqdm(short_test_set)
-        #     for ix, protein in enumerate(test_pbar):
-        #         batch_clones = [copy.deepcopy(protein) for i in range(BATCH_COPIES)]
-        #         X, S, mask, lengths, chain_M, chain_encoding_all, chain_list_list, visible_list_list, masked_list_list, masked_chain_length_list_list, chain_M_pos, omit_AA_mask, residue_idx, dihedral_mask, tied_pos_list_of_lists_list, pssm_coef, pssm_bias, pssm_log_odds_all, bias_by_res_all, tied_beta = CATH_tied_featurize(batch_clones, device, chain_id_dict)
-        #         randn_1 = torch.randn(chain_M.shape, device=X.device)
-        #         log_probs,_ = model(X, S, mask, chain_M*chain_M_pos, residue_idx, chain_encoding_all, randn_1)
-        #         mask_for_loss = mask*chain_M*chain_M_pos
-        #         loss, loss_av = loss_nll(S,log_probs, mask_for_loss)
-
-        #         short_test_sum += torch.sum(loss * mask_for_loss).cpu().data.numpy()
-        #         short_test_weights += torch.sum(mask_for_loss).cpu().data.numpy()
-        #         short_test_recovery.append(calculate_recovery(log_probs,S))
-
-        # with torch.no_grad():
-        #     sc_test_sum, sc_test_weights = 0., 0.
-        #     sc_test_recovery = []
-        #     test_pbar = tqdm(sc_test_set)
-        #     for ix, protein in enumerate(test_pbar):
-        #         batch_clones = [copy.deepcopy(protein) for i in range(BATCH_COPIES)]
-        #         X, S, mask, lengths, chain_M, chain_encoding_all, chain_list_list, visible_list_list, masked_list_list, masked_chain_length_list_list, chain_M_pos, omit_AA_mask, residue_idx, dihedral_mask, tied_pos_list_of_lists_list, pssm_coef, pssm_bias, pssm_log_odds_all, bias_by_res_all, tied_beta = CATH_tied_featurize(batch_clones, device, chain_id_dict)
-        #         randn_1 = torch.randn(chain_M.shape, device=X.device)
-        #                     # model(X, S, mask, chain_M,             residue_idx, chain_encoding_all)
-        #         log_probs, _ = model(X, S, mask, chain_M*chain_M_pos, residue_idx, chain_encoding_all, randn_1)
-        #         mask_for_loss = mask*chain_M*chain_M_pos
-        #         loss, loss_av = loss_nll(S,log_probs, mask_for_loss)
-
-        #         sc_test_sum += torch.sum(loss * mask_for_loss).cpu().data.numpy()
-        #         sc_test_weights += torch.sum(mask_for_loss).cpu().data.numpy()
-        #         sc_test_recovery.append(calculate_recovery(log_probs,S))
-
-
-    #         ## Loads CATH 4.2 Short test dataset
-    # file_splits='/data/project/rw/cath4.2/test_split_L100.json'
-    # with open(file_splits) as f:
-    #     dataset_splits = json.load(f)
-    # short_test_set = Subset(dataset, [dataset_indices[chain_name] for chain_name in dataset_splits['test'] if chain_name in dataset_indices])
-
-    # file_splits='/data/project/rw/cath4.2/test_split_sc.json'
-    # with open(file_splits) as f:
-    #     dataset_splits = json.load(f)
-    # sc_test_set = Subset(dataset, [dataset_indices[chain_name] for chain_name in dataset_splits['test'] if chain_name in dataset_indices])
-    
