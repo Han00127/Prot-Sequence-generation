4ce25361355611de507ae38e73c514bfa1179c52
Kyeongtak (David) Han <93216105+Han00127@users.noreply.github.com>
2023-06-09 16:46:01 +0900

    Update mpnn_cmlm.sh

--- scripts/mpnn_cmlm.sh
+++ scripts/mpnn_cmlm.sh
@@ -6,5 +6,8 @@ splits='./data/chain_set_splits.json'
 shortPath='./data/test_split_L100.json'
 scPath='./data/test_split_sc.json'
 
-## Prev model trained with 4 decoder layers.
-python mpnn_train.py --model MPNN_CMLM --epoch 100 --out_folder $out_folder --jsonl_path $cathDataPath --file_splits $splits --test_short_path $shortPath --test_single_path $scPath --num_decoder_layers 4 
+# ## Training from pretrained model vanilla ProtMPNN model - v_48_020.pt)) encoder decoder 3 / 3 as same as official model
+# python mpnn_train.py --model MPNN_CMLM --epoch 100 --out_folder $out_folder --jsonl_path $cathDataPath --file_splits $splits --test_short_path $shortPath --test_single_path $scPath
+
+## Prev model trained with 4 decoder layers training from scratch
+python mpnn_train.py --model MPNN_CMLM --epoch 100 --out_folder $out_folder --jsonl_path $cathDataPath --file_splits $splits --test_short_path $shortPath --test_single_path $scPath --use_pretrained_weights "" --num_decoder_layers 4 
